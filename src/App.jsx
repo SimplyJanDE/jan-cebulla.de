7a69039dc620469a6280935f1cb01ad2e4d46962
SimplyJanDE <78982850+SimplyJanDE@users.noreply.github.com>
2023-08-18 18:51:46 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import LinkList from './components/LinkList';
-import links from './/components/linksData'; // Importiere die links-Konstante
+import links from './components/linksData'; // Importiere die links-Konstante
 
 function App() {
   return (
