50d5e033bbf0aa979f1494573179756e4e698294
SimplyJanDE <78982850+SimplyJanDE@users.noreply.github.com>
2023-12-02 12:09:03 +0000

    updated CSS for responsive Design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+// tailwind.config.js
 
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html",
+  ],
+  // ...
+};
